c824cbd27b40b433d7a2f14937b7443bb71cde29
Jan Vesely <jan.vesely@n-able.com>
2025-11-10 18:52:11 +0000

    Add icons

--- weather-chart/src/app/app.routes.ts
+++ weather-chart/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { WindChart } from './wind-chart/wind-chart';
 import { TempChart } from './temp-chart/temp-chart';
+import { WeatherStation } from './weather-station/weather-station';
 
 export const routes: Routes = [
   { path: 'wind', component: WindChart },
   { path: 'temp', component: TempChart },
+  { path: 'weather', component: WeatherStation },
   { path: '', redirectTo: '/wind', pathMatch: 'full' }
 ];
