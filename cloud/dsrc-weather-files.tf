718535d7827fd439d7b4cb2bbcc258636520455f
Jan Vesely <jan.vesely@n-able.com>
2025-11-11 17:01:38 +0000

    Update caching

--- cloud/dsrc-weather-files.tf
+++ cloud/dsrc-weather-files.tf
@@ -3,4 +3,13 @@ resource "aws_s3_object" "index" {
   key    = "index.html"
   source = "${path.module}/../www/index.html"  # Local file path
   content_type = "text/html"
+  cache_control = "no-cache, no-store, must-revalidate"
+}
+
+resource "aws_s3_object" "favicon" {
+  bucket        = aws_s3_bucket.dsrc_weather.id
+  key           = "favicon.ico"
+  source        = "${path.module}/../www/favicon.ico"  # Local file path
+  content_type  = "image/x-icon"
+  cache_control = "no-cache, no-store, must-revalidate"
 }
