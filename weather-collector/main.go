28c84db69c52037b7e4911f35b28481562e7ab70
Jan Vesely <jan.vesely@n-able.com>
2026-04-04 13:44:00 +0100

    Fix issue with 8 different time entries

--- weather-collector/main.go
+++ weather-collector/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/veselj/dsrc-weather/internal/record"
@@ -15,6 +16,15 @@ import (
 type Event struct {
 	Count int `json:"count"`
 }
+
+var londonLocation = func() *time.Location {
+	loc, err := time.LoadLocation("Europe/London")
+	if err != nil {
+		log.Printf("Unable to load Europe/London timezone, falling back to UTC: %v", err)
+		return time.UTC
+	}
+	return loc
+}()
 
 func handler(ctx context.Context, event Event) error {
 
@@ -41,6 +51,10 @@ func handler(ctx context.Context, event Event) error {
 		return err
 	}
 
+	if time.Now().In(londonLocation).Hour() < 1 {
+		// Do not update tides between midnight and 1 a.m. London time to avoid confusion with the previous day.
+		return nil
+	}
 	tideTimes, err := tides.Scrape()
 	if err != nil {
 		log.Println("Error scraping tides", err)
